c324d75302c7df80a950bdf7ea11ddd38dfef9f6
ankita-0123 <pragyanparamita1449@gmail.com>
2023-02-19 21:23:06 +0530

    commit id

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,20 +17,36 @@ const ExpenseForm = () => {
 
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const ExpenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(ExpenseData);
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+
+    };
+
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__coontrol">
                     <label>Title</label>
-                    <input type="text" onChange={ titleChangeHandler } />            
+                    <input type="text" value={enteredTitle} onChange={ titleChangeHandler } />            
                 </div>
                 <div className="new-expense__coontrol">
                     <label>Amount</label>
-                    <input type="number" min='0.01' step='0.01' onChange={ amountChangeHandler } />            
+                    <input type="number" min='0.01' step='0.01' value={enteredAmount} onChange={ amountChangeHandler } />            
                 </div>
                 <div className="new-expense__coontrol">
                     <label>Date</label>
-                    <input type="date" onChange={ dateChangeHandler } />            
+                    <input type="date" value={enteredDate} onChange={ dateChangeHandler } />            
                 </div>
             </div>
             <div className="new-expense__actions">
